1a04bb18f11594c64782168fa411f5c747b68d32
Chris <christopherc1331@gmail.com>
2023-07-22 22:56:16 -0500

    Using cors filter bean to enable cors requests for now.
    Need to lock this down in the future. For now this is just for testing

--- src/main/kotlin/xyz/scritto/config/auth/WebConfig.kt
+++ src/main/kotlin/xyz/scritto/config/auth/WebConfig.kt
@@ -1,9 +1,14 @@
 package xyz.scritto.config.auth
 
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import org.springframework.web.filter.CorsFilter
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.EnableWebMvc
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import java.util.Collections
 
 
 @Configuration
@@ -11,5 +16,18 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 class WebConfig : WebMvcConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
+    }
+
+    @Bean
+    fun corsFilter(): CorsFilter? {
+        val source = UrlBasedCorsConfigurationSource()
+        val config = CorsConfiguration()
+        config.allowCredentials = true
+        // Don't do this in production, use a proper list  of allowed origins
+        config.allowedOrigins = Collections.singletonList("*")
+        config.allowedHeaders = listOf("Origin", "Content-Type", "Accept")
+        config.allowedMethods = listOf("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH")
+        source.registerCorsConfiguration("/**", config)
+        return CorsFilter(source)
     }
 }
