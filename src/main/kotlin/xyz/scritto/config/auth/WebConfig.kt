b079406e481c70bf656fb691c857d96dc6d56432
Chris <christopherc1331@gmail.com>
2023-07-23 13:13:04 -0500

    does this work for cors?

--- src/main/kotlin/xyz/scritto/config/auth/WebConfig.kt
+++ src/main/kotlin/xyz/scritto/config/auth/WebConfig.kt
@@ -2,16 +2,16 @@ package xyz.scritto.config.auth
 
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
-import org.springframework.web.servlet.config.annotation.EnableWebMvc
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 
 @Configuration
-@EnableWebMvc
 class WebConfig : WebMvcConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
-            .allowedOrigins("*")
-            .allowedMethods("*")
+            .allowedOrigins("http:localhost:3000", "https://scritto-fe-production.up.railway.app/3000")
+            .allowedMethods("GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS", "HEAD")
+            .allowedHeaders("*")
+            .allowCredentials(true)
     }
 }
