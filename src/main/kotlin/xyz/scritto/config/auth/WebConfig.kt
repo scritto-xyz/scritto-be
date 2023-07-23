7605d3b0bc1585972e62ef633fdd2dbf6be379f9
Chris <christopherc1331@gmail.com>
2023-07-22 23:24:11 -0500

    Hoping this works for cors

--- src/main/kotlin/xyz/scritto/config/auth/WebConfig.kt
+++ src/main/kotlin/xyz/scritto/config/auth/WebConfig.kt
@@ -1,26 +1,16 @@
 package xyz.scritto.config.auth
 
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.web.cors.CorsConfiguration
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource
-import org.springframework.web.filter.CorsFilter
-import java.util.Collections
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 
 @Configuration
-class WebConfig {
-
-    @Bean
-    fun corsFilter(): CorsFilter? {
-        val source = UrlBasedCorsConfigurationSource()
-        val config = CorsConfiguration()
-        config.allowCredentials = true
-        // Don't do this in production, use a proper list  of allowed origins
-        config.allowedOrigins = Collections.singletonList("*")
-        config.allowedHeaders = listOf("Origin", "Content-Type", "Accept")
-        config.allowedMethods = listOf("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH")
-        source.registerCorsConfiguration("/**", config)
-        return CorsFilter(source)
+@EnableWebMvc
+class WebConfig : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*")
     }
 }
