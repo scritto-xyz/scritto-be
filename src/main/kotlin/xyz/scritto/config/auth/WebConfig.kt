827e7c4a560f25bc0a1e1649d37f621cb3b63b03
Chris <christopherc1331@gmail.com>
2023-07-31 23:11:03 -0500

    Allowing all origins for now

--- src/main/kotlin/xyz/scritto/config/auth/WebConfig.kt
+++ src/main/kotlin/xyz/scritto/config/auth/WebConfig.kt
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 class WebConfig : WebMvcConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:3000", "https://scritto-fe-production.up.railway.app:3000")
+            .allowedOriginPatterns("*")
             .allowedMethods("GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS", "HEAD")
             .allowedHeaders("*")
             .allowCredentials(true)
